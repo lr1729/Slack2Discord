d89ddda4477b8eacc29f9ec98e47e25220929d5b
ling1729 <renlingfeng21@gmail.com>
2020-01-24 20:27:47 -0800

    file and image support

--- slacktodiscord.js
+++ slacktodiscord.js
@@ -1,35 +1,25 @@
+//get tokens from enviornment variables
 const DISCORD_TOKEN         = process.env.DISCORD_TOKEN;
-const DISCORD_CHANNEL       = '';
-const DISCORD_CHANNELID     = '670052145583751172';
 const slackSigningSecret    = process.env.SLACK_SIGNING_SECRET;
 const token                 = process.env.SLACK_TOKEN; 
+const bottoken              = process.env.BOT_TOKEN;
 
+//deps
 const Discord = require('discord.js');
+const { createEventAdapter } = require('@slack/events-api');
+const { WebClient } = require('@slack/web-api');
+const web = new WebClient(token);
+const fs = require('fs');
+const fileExtension = require('file-extension');
+const del = require('del');
 const discord_client = new Discord.Client();
+const slackEvents = createEventAdapter(slackSigningSecret);
+const port = process.env.PORT || 3000;
+const request = require('request');
+
 var discord_channel;
 
 discord_client.login(DISCORD_TOKEN);
-
-discord_client.on('ready', function(){
-	//Finds the right channel to send the messages
-	var param = DISCORD_CHANNEL !== "" ? "name" : "id";
-	var value = DISCORD_CHANNEL !== "" ? DISCORD_CHANNEL : DISCORD_CHANNELID;
-	var potential_channels = discord_client.channels.findAll(param, value);
-	if (potential_channels.length === 0) {
-		console.log("Error: No Discord channels with " + param + " " + value + " found.");
-		process.exit(1);
-	}
-	if (potential_channels.length > 1) {
-		console.log("Warning: More than 1 Discord channel with " + param + " " + value + " found.");
-		console.log("Defaulting to first one found");
-	}
-	discord_channel = potential_channels[0];
-	console.log("Discord connected");
-});
-
-const { createEventAdapter } = require('@slack/events-api');
-const slackEvents = createEventAdapter(slackSigningSecret);
-const port = process.env.PORT || 3000;
 
 (async () => {
   const server = await slackEvents.start(port);
@@ -39,6 +29,7 @@ const port = process.env.PORT || 3000;
 slackEvents.on('message',  (async function(message) {
         if (message.type == "message")
         {
+			//get info
 			var name = (await getUser(message.user)).user.name;
 			var channel = (await getChannel(message.channel)).channel.name;
 			var pfp = (await getUser(message.user)).user.profile.image_72;
@@ -59,20 +50,48 @@ slackEvents.on('message',  (async function(message) {
 			console.log("Connected to channel " + channel);
 	
 			//send message
-			
-			var exampleEmbed = new Discord.RichEmbed()
+			var textEmbed = new Discord.RichEmbed()
 				.setAuthor(name, pfp)
-				//.setThumbnail(pfp)
 				.setDescription(message.text);
-			discord_channel.send(exampleEmbed);
+			discord_channel.send(textEmbed);
+			
+			//handle files and images
+			if("files" in message){
+				for(i = 0; i < message.files.length; i++){
+					var fileext = fileExtension([message.files[i].name])
+					//Get file
+					const options = {
+						url: message.files[i].url_private,
+						method: "GET",
+						headers: {
+							"Authorization": "Bearer " + token,
+						}
+					};
+					await new Promise(resolve =>
+						request(options)
+							.pipe(fs.createWriteStream(message.files[i].name))
+							.on('finish', resolve));
+							
+					//inline images
+					if(fileext == "png" || fileext == "jpg"){
+						const file = new Discord.Attachment(message.files[i].name);
+						const imageEmbed = {
+							title: message.files[i].name,
+							image: {
+								url: 'attachment://' + message.files[i].name,
+							},
+						};
+
+						await discord_channel.send({ files: [file], embed: imageEmbed });
+					} else
+						await discord_channel.send({files: [message.files[i].name]});
+					//delete file
+					await del([message.files[i].name]);
+				}
+				console.log("uploaded " + message.files.length + " files");
+			}
         }
 }));
-
-
-const { WebClient } = require('@slack/web-api');
-
-// Initialize web api
-const web = new WebClient(token);
 
 async function getUser(id){
 	const thing = await web.users.info({
@@ -87,7 +106,3 @@ async function getChannel(id){
 	});
 	return thing;
 }
-
-(async () => {
-  console.log((await getUser('UT44N6CT0')).user.name);
-})();
